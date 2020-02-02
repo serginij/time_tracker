22c68e25c1c6835f0d2f4945d710a5071ee8cf1c
Юрий <urij_voit@MacBook-Pro-Urij.local>
2020-02-02 23:19:38 +0300

    feat: add first tests

---
+++ src/components/main/index.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Main } from './index'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+afterEach(cleanup)
+
+describe('test table', () => {
+  test('contains six elements', () => {
+    const sitesTest = [
+      { url: 'first', time: 1000 },
+      {
+        url:
+          'qiwtrwqeirhodsfjhafhdsakjfhdsklahfksadlhfkjdashlfkjdhsafkjhdsakfhdsakjfhdaslkfhdsklfhal',
+        time: 100
+      },
+      { url: '1', time: 110 },
+      { url: 'asf', time: 1 },
+      { url: '234', time: 234 },
+      { url: 'last', time: 112 }
+    ]
+
+    let utils = render(<Main sites={sitesTest} />)
+    let list = utils.getByTestId('list')
+    expect(list.children).toHaveLength(6)
+  })
+
+  test('list is empty', () => {
+    let utils = render(<Main sites={[]} />)
+    let list = utils.getByTestId('list')
+
+    expect(list.children).toHaveLength(0)
+  })
+
+  test('contains six elements', () => {
+    const sitesTest = [{ url: 'first', time: 1005 }]
+
+    let utils = render(<Main sites={sitesTest} />)
+    let list = utils.getByTestId('list')
+
+    expect(list.firstChild.children).toHaveLength(2)
+    expect(list.firstChild.lastChild).toHaveTextContent('16:45')
+  })
+})
